224a178a684ceb142eb07b28a8a45dfcdcebc2a8
keklolovich58-blip <keklolovich58@gmail.com>
2026-03-23 11:10:56 +0300

    Add FurnitureDataModel.h C++ header for furniture data structure

---
+++ Source/FurnitureDataModel.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+struct FurnitureData {
+    int id;
+    std::string name;
+    std::string category;
+    float size;
+    std::string material;
+    float price;
+    std::string texturePath;
+};
